be83ea15680ba7a1fe64a13688559cbb79c9d26a
William <william.jj@hotmail.com>
2020-04-16 23:15:05 +0200

    implemented firebase utils, included ability to store authenticated users into our firestore database

--- src/App.js
+++ src/App.js
@@ -1,47 +1,64 @@
 import React from 'react';
-import {Switch, Route} from "react-router-dom"
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
-import HomePage from "./pages/homepage/homepage.component"
-import ShopPage from "./pages/shop/shop.component.jsx"
-import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component"
-import Header from "./components/header/header.component"
-import {auth} from "./firebase/firebase.utils"
+
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import Header from './components/header/header.component';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor() {
-    super()
+    super();
 
     this.state = {
       currentUser: null
-    }
+    };
   }
 
-  unsubscripeFromAuth = null
+  unsubscribeFromAuth = null;
 
-  componentDidMount(){
-    auth.onAuthStateChanged = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user})
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
-    })
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });   
+          
+          console.log(this.state)
+        });
+      }
+      else{
+        this.setState({ currentUser: userAuth });
+      }
+      
+    });
   }
 
   componentWillUnmount() {
-    this.unsubscripeFromAuth()
+    this.unsubscribeFromAuth();
   }
 
   render() {
-  return (
-    <div>
-      <Header currentUser={this.state.currentUser} />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
-        <Route path="/signin" component={SignInAndSignUp} />
-      </Switch>      
-    </div>
-  );
-}
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInAndSignUpPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
