0c01f51b64631f173cef6819b0d26b591f0ff338
William <william.jj@hotmail.com>
2020-04-20 18:00:21 +0200

    added redux, and implemented userReducer and userActions to header and app components

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom"
+import {Provider} from "react-redux"
+import store from "./redux/store"
 
 ReactDOM.render(
+  <Provider store={store}>
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
